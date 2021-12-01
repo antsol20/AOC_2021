7e476169107481db178bd75f6a085559d5e5f6b7
antsol20 <antsol20@hotmail.com>
2021-12-01 07:07:26 +0000

    done

--- day1.py
+++ day1.py
@@ -1,5 +1,23 @@
-from utils.input import read_input_from_site
+input = []
 
-input_list = read_input_from_site(1)
+f = open('input.txt', 'r')
+
+for line in f:
+    input.append(int(line.strip()))
 
 
+counter = 0
+for i in range(1, len(input)):
+    if input[i] > input[i - 1]:
+        counter += 1
+
+print(counter)
+
+counter = 0
+for i in range(3, len(input)):
+    window1 = input[i] + input[i - 1] + input[i - 2]
+    window2 = input[i - 1] + input[i - 2] + input[i - 3]
+    if window1 > window2:
+        counter += 1
+
+print(counter)
