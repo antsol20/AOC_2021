d992b6e4474de0c8449d56469b9aa86f9f8ebf9d
antsol20 <antsol20@hotmail.com>
2021-12-04 09:20:23 +0000

    x

--- day4.py
+++ day4.py
@@ -50,15 +50,24 @@ def main():
             nums = [int(x) for x in input[i].split()]
             board.append(nums)
 
+    # num_boards = list(range(len(boards)))
+
     for draw in draws:
         for b in range(len(boards)):
             for i in range(5):
                 for j in range(5):
                     if boards[b][j][i] == draw:
                         boards[b][j][i] = 'X'
-                        if check_board_solved(boards[b]):
+                        if b == 64 and check_board_solved(boards[b]):
                             print(get_score(boards[b], draw))
                             return
+                        # if len(num_boards) == 1:
+                        #     print(num_boards)    ## board 64 found to be last
+                        #     return
+                        #
+                        # if check_board_solved(boards[b]):
+                        #     if b in num_boards:
+                        #         num_boards.remove(b)
 
 
 if __name__ == '__main__':
