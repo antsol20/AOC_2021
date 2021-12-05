7db121bdb6b85a7a1d68983421deaf2e94adb6d6
antsol20 <antsol20@hotmail.com>
2021-12-05 07:56:38 +0000

    x

--- day5.py
+++ day5.py
@@ -12,8 +12,6 @@ def main():
         x2, y2 = points[1].split(',')
         input.append((int(x1), int(y1), int(x2), int(y2)))
 
-# 957,596 -> 957,182
-
     for entry in input:
         if entry[0] == entry[2]:  # then vertical
 
@@ -33,6 +31,24 @@ def main():
             if entry[2] > entry[0]:  # then going l2r
                 for i in range(entry[0], entry[2] + 1):
                     grid[entry[1]][i] += 1
+
+        if entry[0] != entry[2] and entry[1] != entry[3]:  # diag
+
+            if entry[0] > entry[2] and entry[3] > entry[1]:
+                for i in range(entry[0] - entry[2] + 1):
+                    grid[entry[0] - i][entry[1] + i] += 1
+
+            if entry[2] > entry[0] and entry[1] > entry[3]:
+                for i in range(entry[2] - entry[0] + 1):
+                    grid[entry[0] + i][entry[1] - i] += 1
+
+            if entry[2] > entry[0] and entry[3] > entry[1]:
+                for i in range(entry[2] - entry[0] + 1):
+                    grid[entry[0] + i][entry[1] + i] += 1
+
+            if entry[0] > entry[2] and entry[1] > entry[3]:
+                for i in range(entry[0] - entry[2] + 1):
+                    grid[entry[0] - i][entry[1] - i] += 1
 
     counter = 0
     for row in grid:
