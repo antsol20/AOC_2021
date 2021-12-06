30794597ff04f4a88526160bdaeb6bb1ef9c2350
antsol20 <antsol20@hotmail.com>
2021-12-06 08:16:02 +0000

    x

---
+++ day6.py
@@ -0,0 +1,22 @@
+from collections import defaultdict, Counter
+
+infile = 'input.txt'
+X = Counter([int(x) for x in open(infile).read().strip().split(',')])
+
+
+def solve(S, n):
+    X = S
+    for day in range(n):
+        Y = defaultdict(int)
+        for x, cnt in X.items():
+            if x == 0:
+                Y[6] += cnt
+                Y[8] += cnt
+            else:
+                Y[x-1] += cnt
+        X = Y
+    return sum(X.values())
+
+
+print(solve(X, 80))
+print(solve(X, 256))
